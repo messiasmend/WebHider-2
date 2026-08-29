873195bd1b824e41080acc2ebe35cae8bcc40c99
messiasmend <36993138+messiasmend@users.noreply.github.com>
2026-08-29 11:37:45 -0300

    Fix 2.3.10 Objective-C hook compile error

--- Sources/PHV2310FinalElementPrompt.m
+++ Sources/PHV2310FinalElementPrompt.m
@@ -1,13 +1,129 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
-@interface PHInspectorViewController:UIViewController
-@property(nonatomic,copy)NSString*currentDetails;@property(nonatomic,copy)NSString*currentSubtitle;-(void)render:(BOOL)hierarchyMode;-(void)backTapped;
+
+@interface PHInspectorViewController : UIViewController
+@property(nonatomic,copy) NSString *currentDetails;
+@property(nonatomic,copy) NSString *currentSubtitle;
+- (void)render:(BOOL)hierarchyMode;
+- (void)backTapped;
 @end
-static NSString*P2310V(NSString*d,NSString*k){for(NSString*l in[d componentsSeparatedByString:@"\n"]){NSString*p=[k stringByAppendingString:@":"];if([l hasPrefix:p])return[[l substringFromIndex:p.length]stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceCharacterSet];}return @"";}
-static NSString*P2310Code(NSString*d){NSString*h=P2310V(d?:@"",@"HTML");if(!h.length)return d?:@"";NSString*t=@"div";NSRange o=[h rangeOfString:@"<"],c=o.location!=NSNotFound?[h rangeOfString:@">" options:0 range:NSMakeRange(o.location,h.length-o.location)]:NSMakeRange(NSNotFound,0);if(o.location!=NSNotFound&&c.location!=NSNotFound){NSString*i=[h substringWithRange:NSMakeRange(o.location+1,c.location-o.location-1)];NSArray*p=[i componentsSeparatedByCharactersInSet:NSCharacterSet.whitespaceCharacterSet];if(p.count&&[p[0]length])t=p[0];}NSMutableString*a=[NSMutableString string];NSString*v=P2310V(d,@"ID");if(v.length)[a appendFormat:@" id=\"%@\"",v];v=P2310V(d,@"Classe");if(v.length)[a appendFormat:@" class=\"%@\"",v];v=P2310V(d,@"Tipo");if(v.length&&![v isEqualToString:t])[a appendFormat:@" type=\"%@\"",v];v=P2310V(d,@"Link");if(v.length)[a appendFormat:@" href=\"%@\"",v];v=P2310V(d,@"Texto");if(v.length)return[NSString stringWithFormat:@"<%@%@>\n    %@\n</%@>",t,a,v,t];return[NSString stringWithFormat:@"<%@%@></%@>",t,a,t];}
-static UILabel*P2310Label(UIView*r){if([r isKindOfClass:UILabel.class])return(UILabel*)r;for(UIView*v in[r.subviews copy]){UILabel*l=P2310Label(v);if(l)return l;}return nil;}
-static UIButton*P2310Button(UIView*r,NSString*t){if([r isKindOfClass:UIButton.class]&&[[((UIButton*)r)titleForState:UIControlStateNormal]isEqualToString:t])return(UIButton*)r;for(UIView*v in[r.subviews copy]){UIButton*b=P2310Button(v,t);if(b)return b;}return nil;}
-static void P2310Refresh(PHInspectorViewController*vc){NSString*s=vc.currentSubtitle?:@"";if([s isEqualToString:@"Filtro JSON"]){UIButton*b=P2310Button(vc.view,@"JSON");if(!b)b=P2310Button(vc.view,@"Voltar");if(b){[b setTitle:@"Voltar" forState:UIControlStateNormal];UIImage*i=[UIImage systemImageNamed:@"chevron.left"];if(i)[b setImage:i forState:UIControlStateNormal];[b removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];[b addTarget:vc action:@selector(backTapped) forControlEvents:UIControlEventTouchUpInside];b.contentHorizontalAlignment=UIControlContentHorizontalAlignmentLeft;b.contentEdgeInsets=UIEdgeInsetsMake(0,18,0,18);b.titleEdgeInsets=UIEdgeInsetsMake(0,8,0,0);}}if(![s isEqualToString:@"Elemento Web selecionado"])return;NSString*c=P2310Code(vc.currentDetails?:@"");if(!c.length)return;for(UIView*o in[vc.view.subviews copy])for(UIView*v in[o.subviews copy])if([v isKindOfClass:UIScrollView.class]){UILabel*l=P2310Label(v);if(l){l.text=c;l.font=[UIFont monospacedSystemFontOfSize:13 weight:UIFontWeightRegular];l.numberOfLines=0;return;}}}
-static void(*P2310OrigS)(id,SEL,NSString*);static void P2310S(id x,SEL c,NSString*d){@try{[x setValue:d?:@"" forKey:@"currentDetails"];[x setValue:@"Elemento Web selecionado" forKey:@"currentSubtitle"];}@catch(__unused NSException*e){}if(P2310OrigS)P2310OrigS(x,c,d);dispatch_async(dispatch_get_main_queue(),^{P2310Refresh((PHInspectorViewController*)x);});}
-static void(*P2310OrigR)(id,SEL,BOOL);static void P2310R(id x,SEL c,BOOL h){if(P2310OrigR)P2310OrigR(x,c,h);dispatch_async(dispatch_get_main_queue(),^{P2310Refresh((PHInspectorViewController*)x);dispatch_async(dispatch_get_main_queue(),^{P2310Refresh((PHInspectorViewController*)x);});});}
-__attribute__((constructor))static void P2310Install(void){dispatch_async(dispatch_get_main_queue(),^{Class c=NSClassFromString(@"PHInspectorViewController");if(!c)return;Method m=class_getInstanceMethod(c,@selector(showSelectedWebElement:));if(m){P2310OrigS=(void*)method_getImplementation(m);if(P2310OrigS!=(IMP)P2310S)method_setImplementation(m,(IMP)P2310S);}m=class_getInstanceMethod(c,@selector(render:));if(m){P2310OrigR=(void*)method_getImplementation(m);if(P2310OrigR!=(IMP)P2310R)method_setImplementation(m,(IMP)P2310R);}});}
+
+static NSString *P2310V(NSString *d, NSString *k) {
+    for (NSString *l in [d componentsSeparatedByString:@"\n"]) {
+        NSString *p = [k stringByAppendingString:@":"];
+        if ([l hasPrefix:p]) return [[l substringFromIndex:p.length] stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceCharacterSet];
+    }
+    return @"";
+}
+
+static NSString *P2310Code(NSString *d) {
+    NSString *h = P2310V(d ?: @"", @"HTML");
+    return h.length ? h : (d ?: @"");
+}
+
+static UIButton *P2310Button(UIView *r, NSString *t) {
+    if ([r isKindOfClass:UIButton.class] && [[(UIButton *)r titleForState:UIControlStateNormal] isEqualToString:t]) return (UIButton *)r;
+    for (UIView *v in [r.subviews copy]) {
+        UIButton *b = P2310Button(v, t);
+        if (b) return b;
+    }
+    return nil;
+}
+
+static UITextView *P2310TextView(UIView *r) {
+    if ([r isKindOfClass:UITextView.class]) return (UITextView *)r;
+    for (UIView *v in [r.subviews copy]) {
+        UITextView *tv = P2310TextView(v);
+        if (tv) return tv;
+    }
+    return nil;
+}
+
+static UILabel *P2310Label(UIView *r) {
+    if ([r isKindOfClass:UILabel.class]) return (UILabel *)r;
+    for (UIView *v in [r.subviews copy]) {
+        UILabel *l = P2310Label(v);
+        if (l) return l;
+    }
+    return nil;
+}
+
+static void P2310AlignBack(PHInspectorViewController *vc) {
+    UIButton *b = P2310Button(vc.view, @"JSON");
+    if (!b) b = P2310Button(vc.view, @"Voltar");
+    if (!b) return;
+    [b setTitle:@"Voltar" forState:UIControlStateNormal];
+    UIImage *i = [UIImage systemImageNamed:@"chevron.left"];
+    if (i) [b setImage:i forState:UIControlStateNormal];
+    [b removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];
+    [b addTarget:vc action:@selector(backTapped) forControlEvents:UIControlEventTouchUpInside];
+    b.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    b.contentEdgeInsets = UIEdgeInsetsMake(0, 18, 0, 18);
+    b.titleEdgeInsets = UIEdgeInsetsMake(0, 8, 0, 0);
+}
+
+static void P2310PutElementCode(PHInspectorViewController *vc) {
+    if (![vc.currentSubtitle isEqualToString:@"Elemento Web selecionado"]) return;
+    NSString *code = P2310Code(vc.currentDetails ?: @"");
+    if (!code.length) return;
+
+    UITextView *tv = P2310TextView(vc.view);
+    if (tv) {
+        tv.text = code;
+        tv.editable = NO;
+        tv.scrollEnabled = YES;
+        tv.font = [UIFont monospacedSystemFontOfSize:13 weight:UIFontWeightRegular];
+        return;
+    }
+
+    UILabel *label = P2310Label(vc.view);
+    if (label) {
+        label.text = code;
+        label.numberOfLines = 0;
+        label.font = [UIFont monospacedSystemFontOfSize:13 weight:UIFontWeightRegular];
+    }
+}
+
+static void P2310Refresh(PHInspectorViewController *vc) {
+    if ([vc.currentSubtitle isEqualToString:@"Filtro JSON"]) P2310AlignBack(vc);
+    P2310PutElementCode(vc);
+}
+
+static void (*P2310OrigS)(id, SEL, NSString *);
+static void P2310S(id x, SEL cmd, NSString *d) {
+    @try {
+        [x setValue:(d ?: @"") forKey:@"currentDetails"];
+        [x setValue:@"Elemento Web selecionado" forKey:@"currentSubtitle"];
+    } @catch (__unused NSException *e) {}
+    if (P2310OrigS) P2310OrigS(x, cmd, d);
+    dispatch_async(dispatch_get_main_queue(), ^{ P2310Refresh((PHInspectorViewController *)x); });
+}
+
+static void (*P2310OrigR)(id, SEL, BOOL);
+static void P2310R(id x, SEL cmd, BOOL hierarchyMode) {
+    if (P2310OrigR) P2310OrigR(x, cmd, hierarchyMode);
+    dispatch_async(dispatch_get_main_queue(), ^{ P2310Refresh((PHInspectorViewController *)x); });
+}
+
+static BOOL P2310Installed = NO;
+
+__attribute__((constructor)) static void P2310Install(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (P2310Installed) return;
+        Class c = NSClassFromString(@"PHInspectorViewController");
+        if (!c) return;
+        P2310Installed = YES;
+
+        Method m = class_getInstanceMethod(c, @selector(showSelectedWebElement:));
+        if (m) {
+            P2310OrigS = (void *)method_getImplementation(m);
+            method_setImplementation(m, (IMP)P2310S);
+        }
+
+        m = class_getInstanceMethod(c, @selector(render:));
+        if (m) {
+            P2310OrigR = (void *)method_getImplementation(m);
+            method_setImplementation(m, (IMP)P2310R);
+        }
+    });
+}
